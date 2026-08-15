271bcec9e42035287c638b15758501b5872e1e9c
t-mine <tomonori.mine.biz@gmail.com>
2026-08-15 23:46:10 +0900

    PlayerSlotの位置を分かりやすくした

--- src/components/PlayerSlot.tsx
+++ src/components/PlayerSlot.tsx
@@ -2,7 +2,14 @@ function PlayerSlot() {
 
     return (
     <>
-        <div>PlayerSlot</div>
+        <div 
+            style={{ 
+                backgroundColor: "blue",
+                width: "100px"
+            }}
+        >
+            PlayerSlot
+        </div>
     </>
     )
 }
